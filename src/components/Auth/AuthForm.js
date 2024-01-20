16ae691e2b12382fb55d2f0e91eca228e0ff12d0
deepakkr14 <deepakbipin142@gmail.com>
2024-01-20 23:29:54 +0530

    first commit

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,69 +1,94 @@
 import { useState, useRef } from 'react';
-import { auth } from './firebase';
 
-import { getAuth, createUserWithEmailAndPassword ,signInWithEmailAndPassword} from "firebase/auth";
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(false);
-  const [isLoading, setLoading] = useState(false);
-const emailInputRef=useRef()
-const  passwordInputRef=useRef()
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+ 
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
-    setLoading(false)
   };
 
-  const submitHandler=(event)=>{
+  const submitHandler = (event) => {
     event.preventDefault();
-    setLoading(true)
-const enteredemail=emailInputRef.current.value; 
-const enteredPassword = passwordInputRef.current.value;
-if(isLogin){
-const auth = getAuth();
-signInWithEmailAndPassword(auth, enteredemail, enteredPassword)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    setLoading(false)
-    console.log("Logged In as: ", user.email);
-    // ...
-  })
-  .catch((error) => {
-    setLoading(false)
-    alert(error.message)
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-}else{
- 
-  const auth = getAuth();
-  createUserWithEmailAndPassword(auth, enteredemail, enteredPassword)
-    .then((userCredential) => {
-      // Signed up 
-      const user = userCredential.user;
-      console.log("User's email is: ", user.email,user);
-      setLoading(false)
-      // ...
-    })
-    .catch((error) => {
-      console.log(error.code,error.message)
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      setLoading(false)
-      alert(error.message)
-      // ..
-    });
-}
 
-  }
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // optional: Add validation
+
+    setIsLoading(true);   
+    if (isLogin) {
+      fetch(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCAddUzy56S_Fd9ynLhR2NrwXQPUB1M2i8',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      ).then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          // ...
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = 'Authentication failed!';
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+            alert(errorMessage);
+          });
+        }
+      });
+    
+    } else {
+      fetch(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCAddUzy56S_Fd9ynLhR2NrwXQPUB1M2i8',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      ).then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          // ...
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = 'Authentication failed!';
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+            alert(errorMessage);
+          });
+        }
+      });
+    }
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form >
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' ref={emailInputRef} required />
+          <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
@@ -75,8 +100,8 @@ signInWithEmailAndPassword(auth, enteredemail, enteredPassword)
           />
         </div>
         <div className={classes.actions}>
-          {isLoading && <p>...sending request</p>}
-       {!isLoading &&  <button onClick={submitHandler}> Submit </button>}
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Sending request...</p>}
           <button
             type='button'
             className={classes.toggle}
